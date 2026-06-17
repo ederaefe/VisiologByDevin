5fc3ad90d5ae741c9e179ebaa54e7910237810d3
Elrazor theodore <elrazortheodore@gmail.com>
2026-06-17 20:15:08 +0100

    Create manifest.js

---
+++ public/data/manifest.js
@@ -0,0 +1,9 @@
+{
+  "name": "BarchScan",
+  "short_name": "BarchScan",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#0b0b0e",
+  "theme_color": "#5b9cf5",
+  "icons": []
+}
